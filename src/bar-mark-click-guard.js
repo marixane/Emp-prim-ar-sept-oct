b20dcdad10a209da7e2e5b669ec591766c4da181
marixane <prof.marwane@gmail.com>
2026-06-29 12:36:14 +0100

    Block photo picker after grading mark drag

--- src/bar-mark-click-guard.js
+++ src/bar-mark-click-guard.js
@@ -1,6 +1,25 @@
+var gradingMarkActive = false;
+
+function isGradingMark(target) {
+  return target && target.closest && target.closest('.bar-mark');
+}
+
+document.addEventListener('mousedown', function (event) {
+  if (isGradingMark(event.target)) {
+    gradingMarkActive = true;
+  }
+}, true);
+
+document.addEventListener('mouseup', function () {
+  if (gradingMarkActive) {
+    setTimeout(function () {
+      gradingMarkActive = false;
+    }, 250);
+  }
+}, true);
+
 document.addEventListener('click', function (event) {
-  var target = event.target;
-  if (target && target.closest && target.closest('.bar-mark')) {
+  if (gradingMarkActive || isGradingMark(event.target)) {
     event.preventDefault();
     event.stopPropagation();
   }
